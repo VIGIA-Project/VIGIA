28a6ab802cee8f3871dead695b08afe236976233
AntonyC12 <coelloantony1212@gmail.com>
2026-05-03 03:16:45 -0500

    build: KAN-4 initialize hexagonal structure and contexts for business logic

---
+++ evaluacion-backend/src/test/java/com/uce/prueba/EvaluacionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

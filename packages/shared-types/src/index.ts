1e6b0b2211a1029de7f90580fbcd9113afe4a95e
AntonyC12 <coelloantony1212@gmail.com>
2026-07-03 05:05:59 -0500

    feat(shared-types): export new domain enums

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,3 +1,7 @@
 export * from './dto/auth.dto';
 export * from './enums/decision.enum';
+export * from './enums/autorizacion.enum';
+export * from './enums/severidad.enum';
+export * from './enums/movimiento.enum';
+export * from './enums/rol.enum';
 export * from './interfaces/api-response.interface';
